7f328b754b1dcf2886e9b389b5be6241e5c0a677
Isaac Maier <padspambox@gmail.com>
2023-02-18 18:29:14 -0500

    Hopefully fix hide-podcasts formula

--- spicetify-hide-podcasts.rb
+++ spicetify-hide-podcasts.rb
@@ -1,12 +1,17 @@
 class SpicetifyHidePodcasts < Formula
-  desc "A Spicetify extension to hide podcasts in Spotify"
+  desc "Spicetify extension to hide podcasts in the Spotify client"
   homepage "https://github.com/theRealPadster/spicetify-hide-podcasts"
   url "https://github.com/theRealPadster/spicetify-hide-podcasts/archive/refs/tags/v2.6.0.zip"
   sha256 "bb40c480b93ddd36be6fa70a91a4afae5845832616464b802efa5d47e907da87"
+  license "GPL-3.0-only"
+
+  depends_on "spicetify-cli"
 
   def install
-    # Extract the hidePodcasts.js file and copy it to the Spicetify extensions directory
-    system "unzip", "-j", "#{name}-#{version}.zip", "spicetify-hide-podcasts-#{version}/hidePodcasts.js", "-d", "#{ENV['HOME']}/.config/spicetify/Extensions"
+    # Copy hidePodcasts.js to Extensions directory
+    extensions_dir = "#{ENV["HOME"]}/.config/spicetify/Extensions"
+    mkdir_p extensions_dir
+    cp "hidePodcasts.js", extensions_dir
   end
 
   test do
